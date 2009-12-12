602a95ecb53b7da7fc1478129b1b9f61394c2627
missdeer <missdeer@959521e0-e54e-11de-9298-fbd6a473cdd1>
2009-12-12 01:09:10 +0000

    (+)add an interface to extract words only match the given leading word

--- trunk/tokenizer/tokenizer.cpp
+++ trunk/tokenizer/tokenizer.cpp
@@ -3,11 +3,15 @@
 #include <iostream>
 #include <fstream>
 #include <boost/tokenizer.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/bind.hpp>
+#include <boost/bind.hpp>
 #include <string>
+
 #define MYNAME		"tokenizer"
 
 #ifndef RELEASE
-#  define RELEASE "0.1"
+#  define RELEASE "0.2"
 #endif
 
 using namespace std;
@@ -47,7 +51,7 @@ static int parse(lua_State* L)
     if (lua_isstring(L, 1))
         strToParse = lua_tostring(L, 1);
     else
-        return pusherror(L, "tokenizer(string [, seperators]);") ;
+        return pusherror(L, "parse(string [, seperators]);") ;
 
     string strSep = "-;|()[]{}+-*/.,\\<>?\":&^%$#@!~`=' \t\r\n";
     if (lua_isstring(L, 2))
@@ -77,13 +81,75 @@ static int parse(lua_State* L)
     
     return 1;
 }
-/**
- * Mappings between the LUA code and our C code.
- */
+
+static bool not_leading_with(string str, string strLeading)
+{
+    string::size_type nPos = str.find(strLeading);
+    return (nPos != 0);
+}
+
+static int extract(lua_State* L)
+{
+    string strToParse;
+    if (lua_isstring(L, 1))
+        strToParse = lua_tostring(L, 1);
+    else
+        return pusherror(L, "extract(string, leading string [, seperators]);") ;
+
+    string strLeading;
+    if (lua_isstring(L, 2))
+        strLeading = lua_tostring(L, 2);
+    else
+        return pusherror(L, "extract(string, leading string [, seperators]);") ;
+
+    string strSep = "-;|()[]{}+-*/.,\\<>?\":&^%$#@!~`=' \t\r\n";
+    if (lua_isstring(L, 3))
+        strSep = lua_tostring(L, 3);
+
+    typedef boost::tokenizer<boost::char_separator<char> > Tok;
+    boost::char_separator<char> sep(strSep.c_str()); // default constructed
+    Tok tok(strToParse, sep);
+    std::vector<std::string> arr;
+    for(Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end(); ++tok_iter)
+        arr.push_back(*tok_iter);
+    std::sort(arr.begin(), arr.end());
+    std::vector<std::string>::iterator it = std::unique(arr.begin(), arr.end());
+    arr.erase(it, arr.end());
+    it = std::find_if( arr.begin(), arr.end(),
+        boost::lambda::_1 > strLeading);
+    if (arr.end() != it && arr.begin() != it)
+    {        
+        cout << "remove to " << *it << endl;
+        arr.erase(arr.begin(), it);
+    }
+
+    it = std::find_if(arr.begin(), arr.end(),
+        boost::bind(not_leading_with, _1, strLeading));
+    if (arr.end() != it)
+    {
+        cout << "remove from " << *it << endl;
+        arr.erase(it, arr.end());
+    }
+
+    lua_newtable (L);
+
+    int index = 0;
+    for (vector<string>::iterator it = arr.begin();
+        arr.end() != it;
+        ++it)
+    {
+        lua_pushnumber (L,index++);
+        lua_pushstring (L, it->c_str());
+        lua_rawset (L, -3);
+    }
+
+    return 1;
+}
 
 static const luaL_reg R[] =
 {
-    {"parse",           parse},
+    {"parse", parse},
+    {"extract", extract},
     {NULL,		NULL}
 };
 
