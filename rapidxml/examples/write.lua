95cd24930174cce8de48bc7f4218a1f82331d2a4
missdeer <missdeer@959521e0-e54e-11de-9298-fbd6a473cdd1>
2010-08-08 01:03:28 +0000

    (+)add examples for rapidxml

---
+++ rapidxml/examples/write.lua
@@ -0,0 +1,23 @@
+require "rapidxml"
+
+local doc = rapidxml.parse( [[<testDoc/>]] )
+local root = doc:root()
+
+for i = 1, 5 do
+    local child = root:append( "testAttribute" )
+    child:set_attr( "index", tostring( i ) )
+end
+
+for i = 1, 5 do
+    local child = root:append( "testText" )
+    child:set_text( tostring( i * 10 ) )
+end
+
+for i = 1, 5 do
+    local child = root:append( "testNode" )
+    child:set_attr( "index", tostring( i*100 ) )
+    child:set_text( tostring( i * 1000 ) )    
+end
+
+doc:save( "rapidxml_test.xml", true )
+
