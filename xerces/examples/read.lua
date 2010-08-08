a59583ca7e5e998a3d56e3d32d5d4b224ad8c651
missdeer <missdeer@959521e0-e54e-11de-9298-fbd6a473cdd1>
2010-08-08 01:07:27 +0000

    (+)add examples for xerces

---
+++ xerces/examples/read.lua
@@ -0,0 +1,16 @@
+require "xerces"
+
+local doc = xerces.open ( "xerces_test.xml" )
+local root = doc:root()
+
+for k,v in pairs(root:children()) do
+    if v:type() == xerces.ELEMENT_NODE then
+        if v:name() == "testAttribute" then
+            print( v:name(), v:attribute( "index" ) )
+            elseif v:name() == "testText" then
+            print( v:name(), v:text() )
+            elseif v:name() == "testNode" then
+            print( v:name(), v:attribute( "index" ), v:text() )
+        end
+    end
+end
